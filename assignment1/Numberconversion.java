23425ec7a7c33434c70730ef1e4f40eb36163d61
sharmijai <sharmij23@gmail.com>
2022-08-06 21:56:41 +0530

    Day1 Assignment1

---
+++ assignment1/Numberconversion.java
@@ -0,0 +1,19 @@
+package week1.assignment1;
+
+public class Numberconversion {
+	public static void main(String[] args) {
+		int c= -10;
+		int d;
+		if(c<0) 
+		{
+			d=(c*(-1));
+			System.out.println("Now The Given Number is Converted To Positive");
+		}
+			
+		else 
+		{
+			System.out.println("Number is Negative");
+		
+	}
+
+	}}
