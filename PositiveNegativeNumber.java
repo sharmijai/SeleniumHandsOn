9fc764ec519d33734cedc89ff3b5eb3573e55e19
sharmijai <sharmij23@gmail.com>
2022-08-06 21:51:40 +0530

    Add files via upload

---
+++ PositiveNegativeNumber.java
@@ -0,0 +1,16 @@
+package week1.assignment1;
+
+public class PositiveNegativeNumber {
+public static void main(String[] args) {
+	int a=-1;
+	if(a>0) 
+	{
+		System.out.println("Number is Positive");
+	}
+	else 
+	{
+		System.out.println("Number is Negative");
+	}
+}
+
+}
